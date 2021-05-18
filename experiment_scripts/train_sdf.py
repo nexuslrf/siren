567e9184d463b7bc25e7a15bd8cad913db617d6b
nexuslrf <shootingstarlrf@hotmail.com>
2021-05-18 16:22:43 +0800

    train sdf

--- experiment_scripts/train_sdf.py
+++ experiment_scripts/train_sdf.py
@@ -19,14 +19,15 @@ p.add_argument('--experiment_name', type=str, required=True,
                help='Name of subdirectory in logging_root where summaries and checkpoints will be saved.')
 
 # General training options
-p.add_argument('--batch_size', type=int, default=1400)
+p.add_argument('--batch_size', type=int, default=1)
+p.add_argument('--points_per_batch', type=int, default=1400)
 p.add_argument('--lr', type=float, default=1e-4, help='learning rate. default=5e-5')
-p.add_argument('--num_epochs', type=int, default=10000,
+p.add_argument('--num_epochs', type=int, default=4000,
                help='Number of epochs to train for.')
 
-p.add_argument('--epochs_til_ckpt', type=int, default=1,
+p.add_argument('--epochs_til_ckpt', type=int, default=1000,
                help='Time interval in seconds until checkpoint is saved.')
-p.add_argument('--steps_til_summary', type=int, default=100,
+p.add_argument('--steps_til_summary', type=int, default=10,
                help='Time interval in seconds until tensorboard summary is saved.')
 
 p.add_argument('--model_type', type=str, default='sine',
@@ -35,17 +36,18 @@ p.add_argument('--point_cloud_path', type=str, default='/home/sitzmann/data/poin
                help='Options are "sine" (all sine activations) and "mixed" (first layer sine, other layers tanh)')
 
 p.add_argument('--checkpoint_path', default=None, help='Checkpoint to trained model.')
+p.add_argument('--split_mlp', action='store_true')
 opt = p.parse_args()
 
 
-sdf_dataset = dataio.PointCloud(opt.point_cloud_path, on_surface_points=opt.batch_size)
-dataloader = DataLoader(sdf_dataset, shuffle=True, batch_size=1, pin_memory=True, num_workers=0)
+sdf_dataset = dataio.PointCloud(opt.point_cloud_path, on_surface_points=opt.points_per_batch)
+dataloader = DataLoader(sdf_dataset, shuffle=True, batch_size=opt.batch_size, pin_memory=True, num_workers=4)
 
 # Define the model.
 if opt.model_type == 'nerf':
-    model = modules.SingleBVPNet(type='relu', mode='nerf', in_features=3)
+    model = modules.SingleBVPNet(type='relu', mode='nerf', in_features=3, split_mlp=opt.split_mlp)
 else:
-    model = modules.SingleBVPNet(type=opt.model_type, in_features=3)
+    model = modules.SingleBVPNet(type=opt.model_type, in_features=3, split_mlp=opt.split_mlp)
 model.cuda()
 
 # Define the loss
@@ -55,6 +57,6 @@ summary_fn = utils.write_sdf_summary
 root_path = os.path.join(opt.logging_root, opt.experiment_name)
 
 training.train(model=model, train_dataloader=dataloader, epochs=opt.num_epochs, lr=opt.lr,
-               steps_til_summary=opt.steps_til_summary, epochs_til_checkpoint=opt.epochs_til_ckpt,
+               steps_til_summary=opt.steps_til_summary * len(sdf_dataset), epochs_til_checkpoint=opt.epochs_til_ckpt,
                model_dir=root_path, loss_fn=loss_fn, summary_fn=summary_fn, double_precision=False,
                clip_grad=True)
